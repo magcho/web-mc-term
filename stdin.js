1ffe244b068c06894d4d6c36dfd99f1763c410cf
magcho <mail@magcho.com>
2022-03-06 15:39:33 +0900

    wip

--- stdin.js
+++ stdin.js
@@ -1,39 +1,54 @@
 import pty from "node-pty";
 import readline from "readline";
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
 process.stdin.resume();
 process.stdin.setEncoding("utf8");
 
-// const command = "docker exec -it e1b6e9746449 bash";
-// const command = "bash";
-// const command = "docker attach sad_perlman";
-// const command = "/usr/bin/env";
-
 const term = pty.spawn("bash", [], {
-  // name: "xterm-256color",
+  name: "xterm-256color",
   cols: 80,
-  rows: 30,
+  rows: 24,
 });
 
-term.on("data", function (data) {
-  console.log(Date.now(), data.trim());
-  // process.stdout.write(data);
+const app = express();
+const server = http.createServer(app);
+const socket = new Server(server);
+
+// serve
+// app.use("/xterm.js", express.static("./node_modules/xterm"));
+app.use("/", express.static("/public_html"));
+// app.get("/test", (req, res) => {
+//   res.json({ hello: "0" });
+// });
+
+// socket
+socket.on("connect", (socket) => {
+  term.on("data", function (data) {
+    console.log(new date.now().toLocalString("ja-JP"), data.trim());
+
+    // process.stdout.write(data);
+  });
 });
 
-// const startupCommand = "docker-compose exec web bash";
-const startupCommand = "docker attach $(docker-compose ps -q web)";
-
-setTimeout(() => {
-  term.write(`${startupCommand}\r`);
-}, 1000);
-
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: console.log,
-});
-reader.on("line", (i) => {
-  term.write(`${i}\r`);
-});
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: console.log,
+// });
+// reader.on("line", (i) => {
+//   term.write(`${i}\r`);
+// });
 // reader.on("close", () => {
 //   console.log("disconected");
 // });
+
+// init
+// const startupCommand = "docker attach $(docker-compose ps -q mc)";
+// term.write(`${startupCommand}\r`);
+
+const port = process.env.PORT || 3000;
+const serve = app.listen(port, () => {
+  console.log(`[SERVER]${serve.address().address}:${serve.address().port}`);
+});
